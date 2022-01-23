8c9ae9a07b3c4e666aa143d16fdce8f306b2a9ed
aidansieg <aidansieg@gmail.com>
2022-01-23 15:44:23 -0500

    added nested forloops to create grid of shrinking towers

--- generate.py
+++ generate.py
@@ -1,6 +1,27 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
+# set dimensions of box
+length = 1
+width = 1
+height = 1
+
+pyrosim.Start_SDF("boxes.sdf")
+
+#pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[length,width,height])
+pyrosim.Send_Cube(name="Box2", pos=[2.5,2.5,0.5], size=[6,6,1])
+for x in range(6):
+    length = 1
+    width = 1
+    height = 1
+    for y in range(6):
+        length = 1
+        width = 1
+        height = 1
+        for i in range(10):
+            length = length * 0.9
+            width = width * 0.9
+            height = height * 0.9
+            pyrosim.Send_Cube(name="Box", pos=[x,y,i+1.5], size=[length,width,height])
+            
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,11 +1,19 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
 
-p.loadSDF("box.sdf")
+# adds floor to sim
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+planeId = p.loadURDF("plane.urdf")
 
-for i in range(1000):
+# adds gravity force for sim
+p.setGravity(0, 0, -9.8)
+
+p.loadSDF("boxes.sdf")
+
+for i in range(2000):
 	p.stepSimulation()
 	time.sleep(1/60)
 	print(i)
