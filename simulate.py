dea42ab24085ef41afd55b8103257d16d64ec3b9
aidansieg <aidansieg@gmail.com>
2022-02-14 17:52:24 -0500

    added motors for front and back legs that move independently

--- simulate.py
+++ simulate.py
@@ -1,7 +1,9 @@
+from cmath import pi
 import pyrosim.pyrosim as pyrosim
 import pybullet as p
 import pybullet_data
 import time
+import random
 import numpy as np
 
 physicsClient = p.connect(p.GUI)
@@ -18,14 +20,34 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = np.zeros(300)
-frontLegSensorValues = np.zeros(300)
+x = np.linspace(0, 2 * pi, 1000)
 
-for i in range(300):
+frontLegAmplitude = pi/4
+frontLegFrequency = 10
+frontLegPhaseOffset = 5
+frontLegTargetAngles = frontLegAmplitude * np.sin(frontLegFrequency * x + frontLegPhaseOffset)
+
+backLegAmplitude = pi/4
+backLegFrequency = 15
+BackLegPhaseOffset = pi/4
+backLegTargetAngles = backLegAmplitude * np.sin(backLegFrequency * x + BackLegPhaseOffset)
+
+backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
+
+# np.save("data/targetAngles.npy", targetAngles)
+# exit()
+
+for i in range(1000):
 	p.stepSimulation()
 	# adds sensor from back leg
 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+	
+	# motor for torso - backleg joint
+	pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, jointName="Torso_BackLeg", controlMode= p.POSITION_CONTROL, targetPosition= backLegTargetAngles[i], maxForce= 100)
+	pyrosim.Set_Motor_For_Joint(bodyIndex= robotId, jointName="Torso_FrontLeg", controlMode= p.POSITION_CONTROL, targetPosition= frontLegTargetAngles[i], maxForce= 100)
+
 	print(i)
 	time.sleep(1/60)
 
